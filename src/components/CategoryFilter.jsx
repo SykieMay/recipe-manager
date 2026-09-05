07831f190e9488263551118b77ceb2e6ee7384ac
SykieMay <smdaroy2024@hccci.edu.ph>
2026-09-05 21:12:23 +0800

    Complete MA1

--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -8,7 +8,7 @@ export default function CategoryFilter({ activeFilter, onFilterChange }) {
           - The active button should look different from the others
             (e.g., "btn-primary" for active, "btn-outline" otherwise).
           - Don't forget the `key` prop! */}
-          {FILTERS.map((filter) => (
+          {/* {FILTERS.map((filter) => (
             <button
               key={filter}
               className={`btn ${activeFilter === filter ? "btn-primary" : "btn-outline"}`}
@@ -16,7 +16,45 @@ export default function CategoryFilter({ activeFilter, onFilterChange }) {
             >
               {filter}
             </button>
-          ))}
+          ))} */}
+          <button
+            key="All"
+            className={`btn ${activeFilter === "All" ? "btn-primary" : "btn-outline"}`}
+            onClick={() => onFilterChange("All")}
+          >
+            All
+          </button>
+
+          <button
+            key="Breakfast"
+            className={`btn ${activeFilter === "Breakfast" ? "btn-primary" : "btn-outline"}`}
+            onClick={() => onFilterChange("Breakfast")}
+          >
+            Breakfast
+          </button>
+
+          <button
+            key="Lunch"
+            className={`btn ${activeFilter === "Lunch" ? "btn-primary" : "btn-outline"}`}
+            onClick={() => onFilterChange("Lunch")}
+          >
+            Lunch
+          </button>
+
+          <button
+            key="Dinner"
+            className={`btn ${activeFilter === "Dinner" ? "btn-primary" : "btn-outline"}`}
+            onClick={() => onFilterChange("Dinner")}
+          >
+            Dinner
+          </button>
+          <button
+            key="Dessert"
+            className={`btn ${activeFilter === "Dessert" ? "btn-primary" : "btn-outline"}`}
+            onClick={() => onFilterChange("Dessert")}
+            >
+              Dessert
+            </button>
     </div>
   );
 }
